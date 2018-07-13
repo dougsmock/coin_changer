a7b3bf830b090e7f41ac4dcec06a9a3d8133c90d
Your Name <you@example.com>
2018-07-13 12:16:23 -0400

    Think I have it figured out, but most of my tests have strange failures. Go figure.

--- coins.rb
+++ coins.rb
@@ -1,28 +1,28 @@
 def coin_changer(amount)
-  change = { :quarter => 0, :dime => 0, :nickel => 0, :penny => 0 }
+  denoms = { :quarter => 0, :dime => 0, :nickel => 0, :penny => 0 }
 
   while amount >= 25 do
-    change[:quarter] += 1
+    denoms[:quarter] += 1
     amount -= 25
   end
 
   while amount >= 10 do
-    change[:dime] += 1
+    denoms[:dime] += 1
     amount -= 10
   end
 
   while amount >= 5 do
-    change[:nickel] += 1
+    denoms[:nickel] += 1
     amount -= 5
   end
 
   while amount > 0 do
-    change[:penny] += 1
+    denoms[:penny] += 1
     amount -= 1
   end
 
 
-
+  denoms
 
 
 end
--- test_coin.rb
+++ test_coin.rb
@@ -3,10 +3,6 @@ require_relative "coins.rb"
 
 class CoinChanger < Minitest::Test
 
-  def test_assert_that_1_equals_1
-    assert_equal(1, 1)
-  end
-
   def test_if_hash_is_class
 		amount = 1
 		assert_equal(Hash, coin_changer(amount).class)
@@ -15,6 +11,41 @@ class CoinChanger < Minitest::Test
   def test_if_quarter_equals_25
     amount = 25
     assert_equal({:quarter => 1}, coin_changer(amount))
+  end
+
+  def test_if_dime_equals_10
+    amount = 10
+    assert_equal({:dime => 1}, coin_changer(amount))
+  end
+
+  def test_if_nickel_equals_5
+    amount = 5
+    assert_equal({:nickel => 1}, coin_changer(amount))
+  end
+
+  def test_if_penny_equals_1
+    amount = 1
+    assert_equal({:penny => 1}, coin_changer(amount))
+  end
+
+  def test_if_2_penny_equals_2
+    amount = 2
+    assert_equal({:penny => 2}, coin_changer(amount))
+  end
+
+  def test_if_2_coins
+    amount = 11
+    assert_equal({:dime => 1, :penny => 1}, coin_changer(amount))
+  end
+
+  def test_if_all_coins
+    amount = 93
+    assert_equal({:quarter => 3, :dime => 1, :nickel => 1, :penny => 3}, coin_changer(amount))
+  end
+
+  def test_if_all_coins_with_blanks
+    amount = 75
+    assert_equal({:quarter => 3, :dime => 0, :nickel => 0, :penny => 0}, coin_changer(amount))
   end
 
 
