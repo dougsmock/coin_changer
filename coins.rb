ef97b532bb7f72e5d59e96ebfa7a15c4584d370b
Your Name <you@example.com>
2018-07-17 21:27:42 -0400

    Trying to fix a bug on HTML side?

--- coins.rb
+++ coins.rb
@@ -1,40 +1,41 @@
 def coin_changer(amount)
   denoms = { quarter: 0, dime: 0, nickel: 0, penny: 0 }
-  while amount >= 25 do
-    denoms[:quarter] += 1
-    amount -= 25
+
+    while amount >= 25 do
+      denoms[:quarter] += 1
+      amount -= 25
+    end
+
+    while amount >= 10 do
+      denoms[:dime] += 1
+      amount -= 10
+    end
+
+    while amount >= 5 do
+      denoms[:nickel] += 1
+      amount -= 5
+    end
+
+    while amount > 0 do
+      denoms[:penny] += 1
+      amount -= 1
+    end
+
+    if denoms[:quarter] == 0
+      denoms.delete(:quarter)
+    end
+
+    if denoms[:dime] == 0
+      denoms.delete(:dime)
+    end
+
+    if denoms[:nickel] == 0
+      denoms.delete(:nickel)
+    end
+
+    if denoms[:penny] == 0
+      denoms.delete(:penny)
+    end
   end
-
-  while amount >= 10 do
-    denoms[:dime] += 1
-    amount -= 10
-  end
-
-  while amount >= 5 do
-    denoms[:nickel] += 1
-    amount -= 5
-  end
-
-  while amount > 0 do
-    denoms[:penny] += 1
-    amount -= 1
-  end
-
-if denoms[:quarter] == 0
-  denoms.delete(:quarter)
-end
-
-if denoms[:dime] == 0
-  denoms.delete(:dime)
-end
-
-if denoms[:nickel] == 0
-  denoms.delete(:nickel)
-end
-
-if denoms[:penny] == 0
-  denoms.delete(:penny)
-end
-
   denoms
 end
